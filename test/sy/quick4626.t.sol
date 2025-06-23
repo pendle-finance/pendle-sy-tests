3ffd9a2c51a2487368613f96e83edff8c1626762
Nghia Pham <ngfamgithub@gmail.com>
2025-06-23 14:13:03 +0700

    Added redeemable option to 4626 quick test

--- test/sy/quick4626.t.sol
+++ test/sy/quick4626.t.sol
@@ -6,6 +6,7 @@ import {
     PendleERC4626UpgSYV2,
     IERC4626
 } from "pendle-sy/core/StandardizedYield/implementations/PendleERC4626UpgSYV2.sol";
+import { PendleERC4626NoRedeemUpgSY } from "pendle-sy/core/StandardizedYield/implementations/PendleERC4626NoRedeemUpgSY.sol";
 import {IStandardizedYield} from "pendle-sy/interfaces/IStandardizedYield.sol";
 
 contract Quick4626Test is SYTest {
@@ -13,21 +14,29 @@ contract Quick4626Test is SYTest {
         vm.createSelectFork("ethereum");
     }
 
-    address public constant ERC4626_TOKEN = 0xe0a80d35bB6618CBA260120b279d357978c42BCE;
+    address public constant ERC4626_TOKEN = 0x6bf183243FdD1e306ad2C4450BC7dcf6f0bf8Aa6;
+    bool public constant IS_REDEEMABLE = false;
 
     function deploySY() internal override {
         vm.startPrank(deployer);
 
-        address logic = address(new PendleERC4626UpgSYV2(ERC4626_TOKEN));
+
+
         sy = IStandardizedYield(
             deployTransparentProxy(
-                logic,
+                deployImpl(),
                 deployer,
                 abi.encodeCall(PendleERC4626UpgSYV2.initialize, ("SY 4626", "SY-4626"))
             )
         );
 
         vm.stopPrank();
+    }
+
+    function deployImpl() internal returns (address) {
+        address logic1 =  address(new PendleERC4626NoRedeemUpgSY(ERC4626_TOKEN));
+        address logic2 = address(new PendleERC4626UpgSYV2(ERC4626_TOKEN));
+        return IS_REDEEMABLE ? logic2 : logic1;
     }
 
     function initializeSY() internal override {
